24f2d4d4df79be5288503f93a6ada70982539939
Jake Deery <jake.deery@aol.com>
2023-01-24 09:40:48 +0000

    Update example.sh

--- jenkins_config/example.sh
+++ jenkins_config/example.sh
@@ -2,8 +2,8 @@
 
 export SYNC_REPO="${WORKSPACE}/kohaclone"
 export DEBS_OUT="${WORKSPACE}/kohadebs"
-export KDD_IMAGE="22.11"
-export KDD_BRANCH="22.11"
+export KDD_IMAGE="master"
+export KDD_BRANCH="master"
 ## build kdd
 wget -O build.pl https://gitlab.com/ptfs-europe/koha-debs-docker/-/raw/${KDD_BRANCH}/jenkins_config/build.pl
 /usr/bin/perl build.pl
