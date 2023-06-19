ab5c5cf0dc346f0b12648bc2db349e3c3851efed
PerplexedTheta <jake.deery@ptfs-europe.com>
2023-06-19 10:57:45 +0100

    Bumped master to bookworm

--- build/pbuilder.sh
+++ build/pbuilder.sh
@@ -57,7 +57,7 @@ cat <<EOF | tee /tmp/koha_pbuilder.sh >/dev/null
     wget -qO - ${REPO}/gpg.asc | gpg --dearmor | tee /usr/share/keyrings/koha.gpg >/dev/null ; \
     echo deb [signed-by=/usr/share/keyrings/koha.gpg] ${REPO}/ ${SUITE} main | tee /etc/apt/sources.list.d/koha.list ; \
     wget -qO - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null ; \
-    echo deb [signed-by=/usr/share/keyrings/nodesource.gpg] http://deb.nodesource.com/node_14.x/ bullseye main | tee /etc/apt/sources.list.d/nodesource.list ; \
+    echo deb [signed-by=/usr/share/keyrings/nodesource.gpg] http://deb.nodesource.com/node_14.x/ bookworm main | tee /etc/apt/sources.list.d/nodesource.list ; \
     wget -qO - https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarn.gpg >/dev/null ; \
     echo deb [signed-by=/usr/share/keyrings/yarn.gpg] http://dl.yarnpkg.com/debian/ stable main | tee /etc/apt/sources.list.d/yarn.list ; \
     apt clean ; apt update ; \
