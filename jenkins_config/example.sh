79d42d0802ae693d25f06f5fb3022f65008e7f15
Jake Deery <jake.deery@ptfs-europe.com>
2023-09-21 02:29:07 +0100

    Roll-up for 22.11 release

--- jenkins_config/example.sh
+++ jenkins_config/example.sh
@@ -5,8 +5,8 @@ cd ${WORKSPACE}/kohaclone
 
 export SYNC_REPO="${WORKSPACE}/kohaclone"
 export DEBS_OUT="${WORKSPACE}/kohadebs"
-export KDD_IMAGE="master"
-export KDD_BRANCH="master"
+export KDD_IMAGE="22.11-amd64"
+export KDD_BRANCH="22.11"
 ## build kdd
 wget -O build.pl https://gitlab.com/ptfs-europe/koha-debs-docker/-/raw/${KDD_BRANCH}/jenkins_config/build.pl
 /usr/bin/perl build.pl
