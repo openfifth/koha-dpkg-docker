6c65fe42a9738862c2660590a9cd8a625b8be035
Jake Deery <jake.deery@ptfs-europe.com>
2024-11-28 14:03:28 +0000

    Roll-up for 24.11 release

--- jenkins_config/example.sh
+++ jenkins_config/example.sh
@@ -5,8 +5,8 @@ cd ${WORKSPACE}/kohaclone
 
 export SYNC_REPO="${WORKSPACE}/kohaclone"
 export DEBS_OUT="${WORKSPACE}/kohadebs"
-export KDD_IMAGE="main"
-export KDD_BRANCH="main"
+export KDD_IMAGE="24.11"
+export KDD_BRANCH="24.11"
 ## build kdd
 wget -O build.pl https://gitlab.com/openfifth/koha-debs-docker/-/raw/${KDD_BRANCH}/jenkins_config/build.pl
 /usr/bin/perl build.pl
